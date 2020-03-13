fac1f44df704a733b2b7214b3a0aa262f2f62b96
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2020-03-13 01:28:43 -0300

    Incluye apartado para ejecución en Databricks

--- src/test/scala/samples/TEST_Tablas.scala
+++ src/test/scala/samples/TEST_Tablas.scala
@@ -12,8 +12,8 @@ class AppTest {
     val args: Array[String] = new Array[String](1)
     args(0) = "Environment=production,RegisterInControl=false,TestPlanMode=true"
       
-    val huemulLib = new huemul_BigDataGovernance("Pruebas Inicialización de Clases",args,com.yourcompany.settings.globalSettings.Global)
-    val Control = new huemul_Control(huemulLib,null, huemulType_Frequency.MONTHLY)
+    //val huemulLib = new huemul_BigDataGovernance("Pruebas Inicialización de Clases",args,com.yourcompany.settings.globalSettings.Global)
+    //val Control = new huemul_Control(huemulLib,null, huemulType_Frequency.MONTHLY)
       
      @Test
     def OK() = assertTrue(true)
